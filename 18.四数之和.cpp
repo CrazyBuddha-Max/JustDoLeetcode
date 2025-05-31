15892bd744ecca621e5a9db4f30e52fd0a8c0174
CrazyBuddha-Max <1127413498@qq.com>
2025-05-31 17:42:46 +0800

    modified:   "18.\345\233\233\346\225\260\344\271\213\345\222\214.cpp"

--- 18.四数之和.cpp
+++ 18.四数之和.cpp
@@ -5,11 +5,24 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> fourSum(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        if (n < 4)
+        {
+            return ans;
+        }
+        for (int i = 0; i < n -3; i++)
+        {
+            
+        }
+        
         
     }
 };
 // @lc code=end
-
