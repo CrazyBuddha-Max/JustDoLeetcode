5c437314ca42139d83ebc0b2467e56e28508fab4
CrazyBuddha-Max <1127413498@qq.com>
2025-06-01 13:12:43 +0800

    modified:   "更新四数之和"

--- 18.四数之和.cpp
+++ 18.四数之和.cpp
@@ -1,10 +1,4 @@
-/*
- * @lc app=leetcode.cn id=18 lang=cpp
- *
- * [18] 四数之和
- */
-
-// @lc code=start
+// @lc code=star
 class Solution
 {
 public:
@@ -17,12 +11,61 @@ public:
         {
             return ans;
         }
-        for (int i = 0; i < n -3; i++)
+        for (int i = 0; i < n - 3; i++)
         {
-            
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            if ((long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target)
+            {
+                break;
+            }
+            if ((long)nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target)
+            {
+                continue;
+            }
+            for (int j = i + 1; j < n - 2; j++)
+            {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                {
+                    continue;
+                }
+                if ((long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target)
+                {
+                    break;
+                }
+                if ((long)nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target)
+                {
+                    continue;
+                }
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right)
+                {
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target)
+                    {
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left + 1])
+                        {
+                            left++;
+                        }
+                        left++;
+                        while (right > left && nums[right] == nums[right - 1])
+                        {
+                            right--;
+                        }
+                        right--;
+                    }
+                    else
+                    {
+                        sum < target ? left++ : right--;
+                    }
+                }
+            }
         }
-        
-        
+        return ans;
     }
 };
 // @lc code=end
