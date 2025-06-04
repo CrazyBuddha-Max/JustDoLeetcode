f42a60e58becc0343cb97519a5f7366853c0664b
CrazyBuddha-Max <1127413498@qq.com>
2025-06-04 10:33:16 +0800

    commit "移除元素"

--- 27.移除元素.cpp
+++ 27.移除元素.cpp
@@ -8,6 +8,19 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        int n =nums.size();
+        int fast = 0,slow = 0;
+        
+        while (fast < n)
+        {
+            if (nums[fast] !=val)
+            {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
         
     }
 };
